cf610f8085eec70ff6bf139ec20db3ad16a42048
Simon Hensel <simon.hensel@protonmail.com>
2021-05-26 11:58:40 +0200

    new wallpaper
    Former-commit-id: 7f4e79f07a8cc4397c84040fd0dd27c54010f181

--- awesome/themes/multicolor/theme.lua
+++ awesome/themes/multicolor/theme.lua
@@ -16,8 +16,7 @@ local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
 local theme                                     = {}
 theme.confdir                                   = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
---theme.wallpaper                                 = os.getenv("HOME") .. "/.config/awesome/themes/wallpapers/girl-sunset.jpg"
-theme.wallpaper                                 = os.getenv("HOME") .. "/.config/awesome/themes/wallpapers/girl-sunset.jpg"
+theme.wallpaper                                 = os.getenv("HOME") .. "/.config/awesome/themes/wallpapers/campfire.jpg"
 theme.font                                      = "Roboto Mono 10"
 theme.menu_bg_normal                            = "#000000"
 theme.menu_bg_focus                             = "#000000"
